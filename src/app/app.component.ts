08608e4cd9656f5f6fbad32a78bbad7003a5154f
agm234 <pokichcoll@gmail.ru>
2021-08-18 12:57:43 +0300

    refactor: change app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'YouTubeClientApp';
-
-  searchStr:string;
-
-  constructor() {
-    this.searchStr = '';
-  }
 }
